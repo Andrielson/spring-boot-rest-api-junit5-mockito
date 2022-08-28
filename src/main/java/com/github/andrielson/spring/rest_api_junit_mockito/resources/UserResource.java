cde7c004a8c53ba07fa3c03e5f3daf2a566f58d5
Andrielson Silva <andrielson@gmail.com>
2022-08-28 09:49:21 -0300

    Refactor User resource

--- src/main/java/com/github/andrielson/spring/rest_api_junit_mockito/resources/UserResource.java
+++ src/main/java/com/github/andrielson/spring/rest_api_junit_mockito/resources/UserResource.java
@@ -1,18 +1,23 @@
 package com.github.andrielson.spring.rest_api_junit_mockito.resources;
 
 import com.github.andrielson.spring.rest_api_junit_mockito.domain.User;
+import com.github.andrielson.spring.rest_api_junit_mockito.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("user")
 public class UserResource {
 
+    private final UserService userService;
+
     @GetMapping("{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Andrielson", "andrielson@email.com", "password"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
