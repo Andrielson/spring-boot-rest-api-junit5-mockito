29020009974507e04353f42e7c94040d99e19851
Andrielson Silva <andrielson@gmail.com>
2022-08-28 09:21:19 -0300

    Add User entity

---
+++ src/main/java/com/github/andrielson/spring/rest_api_junit_mockito/domain/User.java
@@ -0,0 +1,27 @@
+package com.github.andrielson.spring.rest_api_junit_mockito.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
