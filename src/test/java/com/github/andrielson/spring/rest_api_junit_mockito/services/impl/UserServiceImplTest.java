c101dafaa685723f2fcc924d24c92c1ac8f0f1c5
Andrielson Silva <andrielson@gmail.com>
2022-08-28 14:09:11 -0300

    Test UserServiceImpl findById

---
+++ src/test/java/com/github/andrielson/spring/rest_api_junit_mockito/services/impl/UserServiceImplTest.java
@@ -0,0 +1,81 @@
+package com.github.andrielson.spring.rest_api_junit_mockito.services.impl;
+
+import com.github.andrielson.spring.rest_api_junit_mockito.domain.User;
+import com.github.andrielson.spring.rest_api_junit_mockito.domain.dto.UserDto;
+import com.github.andrielson.spring.rest_api_junit_mockito.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    private UserServiceImpl userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+    private UserDto userDto;
+    private Optional<User> optionalUser;
+
+    @BeforeEach
+    void setUp() {
+        openMocks(this);
+        userService = new UserServiceImpl(userRepository, mapper);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnUserInstance() {
+        Mockito.when(userRepository.findById(anyInt())).thenReturn(optionalUser);
+        var response = userService.findById(user.getId());
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(user.getId(), response.getId());
+        assertEquals(user.getName(), response.getName());
+        assertEquals(user.getEmail(), response.getEmail());
+        assertEquals(user.getPassword(), response.getPassword());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        var id = 1;
+        var name = "User One";
+        var email = "user_one@mail.com";
+        var password = "password";
+
+        user = new User(id, name, email, password);
+        userDto = new UserDto(id, name, email, password);
+        optionalUser = Optional.of(new User(id, name, email, password));
+    }
+}
