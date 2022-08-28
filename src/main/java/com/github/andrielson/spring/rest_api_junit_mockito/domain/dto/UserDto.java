105645fdf18255fce9256ce16b5da42504b9b1da
Andrielson Silva <andrielson@gmail.com>
2022-08-28 10:10:37 -0300

    Add User DTO

---
+++ src/main/java/com/github/andrielson/spring/rest_api_junit_mockito/domain/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.github.andrielson.spring.rest_api_junit_mockito.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
