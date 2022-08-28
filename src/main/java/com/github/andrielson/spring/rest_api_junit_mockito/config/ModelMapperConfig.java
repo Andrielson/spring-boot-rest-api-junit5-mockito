7e6e0cf36359bdbf400872a14ed388aa4a8aa1e8
Andrielson Silva <andrielson@gmail.com>
2022-08-28 10:20:39 -0300

    Add ModelMapper bean

---
+++ src/main/java/com/github/andrielson/spring/rest_api_junit_mockito/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.github.andrielson.spring.rest_api_junit_mockito.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+}
