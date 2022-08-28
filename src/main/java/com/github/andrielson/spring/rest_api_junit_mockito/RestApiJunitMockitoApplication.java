d60112580996e99c126cf95be6ed95b7fb953551
Andrielson Silva <andrielson@gmail.com>
2022-08-28 09:13:01 -0300

    Create project with Spring Initializr

---
+++ src/main/java/com/github/andrielson/spring/rest_api_junit_mockito/RestApiJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.github.andrielson.spring.rest_api_junit_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiJunitMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/andrielson/spring/rest_api_junit_mockito/RestApiJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andrielson.spring.rest_api_junit_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiJunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
