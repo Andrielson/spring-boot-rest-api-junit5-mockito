1e94d777c5f1e99a214b9893cc2dba0f424cc9c5
Andrielson Silva <andrielson@gmail.com>
2022-08-28 13:45:30 -0300

    Test main class

--- src/test/java/com/github/andrielson/spring/rest_api_junit_mockito/RestApiJunitMockitoApplicationTests.java
+++ src/test/java/com/github/andrielson/spring/rest_api_junit_mockito/RestApiJunitMockitoApplicationTests.java
@@ -3,11 +3,12 @@ package com.github.andrielson.spring.rest_api_junit_mockito;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest()
 class RestApiJunitMockitoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void mainTest() {
+        RestApiJunitMockitoApplication.main(new String[]{});
+    }
 
 }
